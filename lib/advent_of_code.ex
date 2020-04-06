5d9a4672e5d9a5b876e3aabcd02f476d77797367
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2020-04-06 20:58:19 -0300

    Implement day 1 challenge 2

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -5,4 +5,7 @@ defmodule AdventOfCode do
 
   @spec day1_challenge1(integer) :: integer
   def day1_challenge1(mass), do: AdventOfCode.Day1.challenge1(mass)
+
+  @spec day1_challenge2(integer) :: integer
+  def day1_challenge2(mass), do: AdventOfCode.Day1.challenge2(mass)
 end
--- lib/advent_of_code/day1.ex
+++ lib/advent_of_code/day1.ex
@@ -14,5 +14,29 @@ defmodule AdventOfCode.Day1 do
     |> Enum.sum()
   end
 
+  @doc """
+  Challenge 2 from day 1
+  """
+  def challenge2(mass_list) do
+    mass_list
+    |> Enum.map(&recursive_mass_calculation/1)
+    |> Enum.sum()
+  end
+
   defp mass_calculation(mass), do: Integer.floor_div(mass, 3) - 2
+
+  defp recursive_mass_calculation(current_mass) do
+    current_mass
+    |> mass_calculation()
+    |> recursive_mass_calculation([])
+    |> Enum.sum()
+  end
+
+  defp recursive_mass_calculation(current_mass, fuels) when current_mass > 0 do
+    current_mass
+    |> mass_calculation()
+    |> recursive_mass_calculation([current_mass | fuels])
+  end
+
+  defp recursive_mass_calculation(_current_mass, fuels), do: fuels
 end
